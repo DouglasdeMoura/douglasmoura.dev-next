f077b2e600ef0805647752c1acaaa0cbad3ed483
Douglas Moura <douglas.ademoura@gmail.com>
2024-09-19 10:19:17 -0300

    style: add padding for mobile devices

--- src/template/components/template.tsx
+++ src/template/components/template.tsx
@@ -3,9 +3,11 @@ import { Header } from './header.js'
 
 export function Template({ children }: PropsWithChildren) {
   return (
-    <div className="m-auto max-w-[960px]">
-      <Header />
-      <main className="flex flex-col gap-8">{children}</main>
+    <div className="px-4">
+      <div className="m-auto max-w-[960px]">
+        <Header />
+        <main className="flex flex-col gap-8">{children}</main>
+      </div>
     </div>
   )
 }
