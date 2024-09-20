1588537f6702bcaa2a31331f662050b8bac2518d
Douglas Moura <douglas.ademoura@gmail.com>
2024-09-20 16:50:54 -0300

    style: add margin from the main body

--- src/template/components/footer.tsx
+++ src/template/components/footer.tsx
@@ -2,7 +2,7 @@ import { SocialLinks } from './social-links'
 
 export function Footer() {
   return (
-    <footer className="border-t py-4 text-base text-center flex justify-between">
+    <footer className="border-t py-4 text-base text-center flex justify-between mt-4">
       <p className="font-bold">Douglas Moura</p>
       <SocialLinks />
     </footer>
