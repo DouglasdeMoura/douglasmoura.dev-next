a96349c06077ce42a1be6fba3edeff0d677c01fa
Douglas Moura <douglas.ademoura@gmail.com>
2024-09-27 11:39:59 -0300

    style: improve the appeareance of kbd tag

--- src/template/components/post.tsx
+++ src/template/components/post.tsx
@@ -46,7 +46,7 @@ export function Post({
       </header>
       <Tags tags={tags} />
       <div
-        className="prose-code:before:content-none prose-code:after:content-none prose prose-a:text-blue-600 hover:prose-a:no-underline"
+        className="prose-code:before:content-none prose-code:after:content-none prose prose-a:text-blue-600 hover:prose-a:no-underline prose-kbd:inline-flex  prose-kbd:items-center prose-kbd:justify-center prose-kbd:gap-1 prose-kbd:min-w-[30px]"
         // biome-ignore lint/security/noDangerouslySetInnerHtml: the content is stored as HTML
         dangerouslySetInnerHTML={{ __html: content }}
       />
