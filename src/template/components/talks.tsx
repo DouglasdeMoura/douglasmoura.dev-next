98b3f1021a1d29864b1baaadca2cc2b06293ceef
Douglas Moura <douglas.ademoura@gmail.com>
2024-09-27 16:41:20 -0300

    chore: order the talks

--- src/template/components/talks.tsx
+++ src/template/components/talks.tsx
@@ -24,24 +24,26 @@ export function Talks({ list }: TalkProps) {
       </header>
 
       <div className="prose-code:before:content-none prose-code:after:content-none prose prose-a:text-blue-600 hover:prose-a:no-underline prose-kbd:inline-flex  prose-kbd:items-center prose-kbd:justify-center prose-kbd:gap-1 prose-kbd:min-w-[30px] prose-p:m-0">
-        {Object.keys(list).map((year) => (
-          <div key={year}>
-            <h2 className="text-xl">{year}</h2>
+        {Object.keys(list)
+          .sort((a, b) => +b - +a)
+          .map((year) => (
+            <div key={year}>
+              <h2 className="text-xl">{year}</h2>
 
-            {list[+year].map((talk) => (
-              <article key="" className="mb-4">
-                <header>
-                  <h3 className="mb-2 mt-0 text-lg">{talk.title}</h3>
-                </header>
+              {list[+year].map((talk) => (
+                <article key="" className="mb-4">
+                  <header>
+                    <h3 className="mb-2 mt-0 text-base">{talk.title}</h3>
+                  </header>
 
-                <p className="text-sm">
-                  <a href={talk.link}>{talk.event}</a>
-                </p>
-                <DateTime created={talk.date} updated={talk.date} />
-              </article>
-            ))}
-          </div>
-        ))}
+                  <p className="text-sm">
+                    <a href={talk.link}>{talk.event}</a>
+                  </p>
+                  <DateTime created={talk.date} updated={talk.date} />
+                </article>
+              ))}
+            </div>
+          ))}
       </div>
     </section>
   )
